460e56b99804bf08033a6f7a82774cb0435c7de9
hassan.waris <hassan.waris@visionetsystems.com>
2025-01-30 16:15:48 +0500

    added open-api

--- ProductMicroservice/src/main/java/com/systems/product/controller/ProductController.java
+++ ProductMicroservice/src/main/java/com/systems/product/controller/ProductController.java
@@ -2,6 +2,12 @@ package com.systems.product.controller;
 
 import java.util.List;
 
+import com.systems.product.model.Product;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,7 +34,15 @@ public class ProductController {
 		ProductResponse response = this.productService.createProduct(productRequest);
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
-	
+	@Operation(summary = "Get a product by its id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Found the product",
+					content = { @Content(mediaType = "application/json",
+							schema = @Schema(implementation = Product.class)) }),
+			@ApiResponse(responseCode = "400", description = "Invalid id supplied",
+					content = @Content),
+			@ApiResponse(responseCode = "404", description = "Product not found",
+					content = @Content) })
 	@GetMapping(path = "/products")
 	public ResponseEntity<List<ProductResponse>> getAllProducts(){
 		List<ProductResponse> allProducts = this.productService.getAllProducts();
